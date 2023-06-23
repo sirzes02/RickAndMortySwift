77bdf7afb88e2c63f14503689ad2f0cc7d5213a5
sirzes02 <sirzes02@gmail.com>
2023-06-23 00:29:45 -0500

    complete character list creation

---
+++ RickAndMorty/ViewsModels/CharacterListViewViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  CharacterListViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Santiago Varela on 22/06/23.
+//
+
+import UIKit
+
+final class CharacterListViewViewModel: NSObject {
+    func fetchCharacters() {
+        RMService.shared.execute(.listCharactersRequests, expecting: RMGetAllCharactersResponse.self) { result in
+            switch result {
+            case .success(_):
+                print("")
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
+    }
+}
+
+extension CharacterListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionviewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        let width = (bounds.width - 30) / 2
+        
+        return CGSize(width: width, height: width * 1.5)
+    }
+}

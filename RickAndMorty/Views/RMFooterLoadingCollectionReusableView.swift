00855d060e437ee926f8ef957b6923a946f42263
sirzes02 <sirzes02@gmail.com>
2023-06-23 14:26:11 -0500

    complete pagination indicator

---
+++ RickAndMorty/Views/RMFooterLoadingCollectionReusableView.swift
@@ -0,0 +1,44 @@
+//
+//  RMFooterLoadingCollectionReusableView.swift
+//  RickAndMorty
+//
+//  Created by Santiago Varela on 23/06/23.
+//
+
+import UIKit
+
+class RMFooterLoadingCollectionReusableView: UICollectionReusableView {
+    static let identifier = "RMFooterLoadingCollectionReusableView"
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        addSubviews(spinner)
+        addConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+    private func addConstraint() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    public func startAnimating() {
+        spinner.startAnimating()
+    }
+}

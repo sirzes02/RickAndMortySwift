3261f1cd7d028ffe44507557872fb193b1b0fc0f
sirzes02 <sirzes02@gmail.com>
2023-06-25 15:47:47 -0500

    complete character episode cell

---
+++ RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RMEpisodeDetailViewController.swift
+//  RickAndMorty
+//
+//  Created by Santiago Varela on 25/06/23.
+//
+
+import UIKit
+
+/// VC to show details about single episode
+final class RMEpisodeDetailViewController: UIViewController {
+    private let url: URL?
+    
+    // MARK: - Init
+    
+    init(url: URL?) {
+        self.url = url
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Episode"
+        view.backgroundColor = .systemGreen
+    }
+}

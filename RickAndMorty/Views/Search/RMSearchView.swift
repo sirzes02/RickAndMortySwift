acfbe213963fbfdd731073878880a6a9910d6cd7
sirzes02 <sirzes02@gmail.com>
2023-07-05 23:15:21 -0500

    complete Search Bar

--- RickAndMorty/Views/Search/RMSearchView.swift
+++ RickAndMorty/Views/Search/RMSearchView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class RMSearchView: UIView {
     private let viewModel: RMSearchViewViewModel
+    private let searchInputView = RMSearchInputView()
     private let noResultsView = RMNoSearchResultsView()
     
     // MARK: - Init
@@ -18,8 +19,9 @@ final class RMSearchView: UIView {
         super.init(frame: frame)
         backgroundColor = .systemBackground
         translatesAutoresizingMaskIntoConstraints = false
-        addSubviews(noResultsView)
+        addSubviews(noResultsView, searchInputView)
         addConstraints()
+        searchInputView.configure(with: RMSearchInputViewViewModel(type: viewModel.config.type))
     }
     
     required init?(coder: NSCoder) {
@@ -28,6 +30,13 @@ final class RMSearchView: UIView {
     
     func addConstraints() {
         NSLayoutConstraint.activate([
+            // Search input view
+            searchInputView.topAnchor.constraint(equalTo: topAnchor),
+            searchInputView.leftAnchor.constraint(equalTo: leftAnchor),
+            searchInputView.rightAnchor.constraint(equalTo: rightAnchor),
+            searchInputView.heightAnchor.constraint(equalToConstant: 110),
+            
+            // No results
             noResultsView.widthAnchor.constraint(equalToConstant: 150),
             noResultsView.heightAnchor.constraint(equalToConstant: 150),
             noResultsView.centerXAnchor.constraint(equalTo: centerXAnchor),

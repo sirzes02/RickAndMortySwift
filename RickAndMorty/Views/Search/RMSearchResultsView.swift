c97c03ccd870a0409676072278823c4ba6e2affa
sirzes02 <sirzes02@gmail.com>
2023-08-27 13:00:50 -0500

    Complete Collection Search Insertion

--- RickAndMorty/Views/Search/RMSearchResultsView.swift
+++ RickAndMorty/Views/Search/RMSearchResultsView.swift
@@ -246,8 +246,24 @@ extension RMSearchResultsView: UIScrollViewDelegate {
             
             if offset >= (totalContentHeight - totalScrollViewFixedHeight - 120) {
                 viewModel.fetchAdditionalResults { [weak self] newResults in
-                    self?.tableView.tableFooterView = nil
-                    self?.collectionViewCellViewModels = newResults
+                    guard let strongSelf = self else {
+                        return
+                    }
+                    
+                    DispatchQueue.main.sync {
+                        strongSelf.tableView.tableFooterView = nil
+                        
+                        let originalCount = strongSelf.collectionViewCellViewModels.count
+                        let newCount = (newResults.count - originalCount)
+                        let total = originalCount + newCount
+                        let startingIndex = total - newCount
+                        let indexPathsToAdd: [IndexPath] = Array(startingIndex..<(startingIndex+newCount)).compactMap({
+                            return IndexPath(row: $0, section: 0)
+                        })
+                        
+                        strongSelf.collectionView.insertItems(at: indexPathsToAdd)
+                        strongSelf.collectionViewCellViewModels = newResults
+                    }
                 }
             }
             t.invalidate()
